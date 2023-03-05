e5d8fe69f020daa125bbb514f22bfde615211015
dungxtd <dungxtd20@gmail.com>
2023-03-05 20:44:24 +0700

    delete cache

--- virtual machine/master/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["racc",["~> 1.4"]],["nokogiri",[">= 0"]],["vagrant-parallels",["= 2.2.6"]],["vagrant-vmware-desktop",["= 3.0.1"]]],"checksum":"b6893709129231255f28aeed4536c98b349b8fb8749dd6763615ac11fb43d571","vagrant_version":"2.3.4"}
